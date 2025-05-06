f472f2f403b0b3b706bd984b7281d6b8285f740a
lasithLRI <lasith.hettiarachchi.dev@gmail.com>
2025-05-07 03:50:15 +0530

    - Product page Navigations issue solved

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,19 +1,11 @@
-interface ProductPageProps {
-    params: {
-      id: string;
-    };
-  }
-  
-  export default async function ProductPage(props: Readonly<ProductPageProps>) {
+import { use } from "react";
 
-    const params = await props.params;
-
-    const { id } = params;
-  
-    return (
-      <div className="w-screen h-screen flex justify-center items-center flex-col text-2xl">
-        <p>Product page</p>
-        <p>Product ID: {id}</p>
-      </div>
-    );
-  }
+export default function ProductPage({params}: {params: Promise<{ id: string }>}) {
+  const { id } = use(params);
+  return (
+    <div className="w-screen h-screen flex justify-center items-center flex-col text-2xl">
+      <p>Product page</p>
+      <p>Product ID: {id}</p>
+    </div>
+  );
+}
