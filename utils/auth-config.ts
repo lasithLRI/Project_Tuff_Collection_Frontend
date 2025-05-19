aca2cb431cfbb5b40f9a1a1fbd0aadd2c9acf571
lasithLRI <lasith.hettiarachchi.dev@gmail.com>
2025-05-19 21:35:57 +0530

    - Token saving problem - addressed.-3

--- utils/auth-config.ts
+++ utils/auth-config.ts
@@ -2,9 +2,9 @@ import { AuthReactConfig } from "@asgardeo/auth-react";
 const baseUrl = "https://api.asgardeo.io/t/tuff";
 
 export const authConfig:AuthReactConfig = {
-  signInRedirectURL: "http://localhost:3000",
-  signOutRedirectURL: "http://localhost:3000/logout",
-  clientID: "YOUR_CLIENT_ID",
+  signInRedirectURL: "https://tuffcollection.netlify.app/",
+  signOutRedirectURL: "https://tuffcollection.netlify.app/",
+  clientID: process.env.NEXT_PUBLIC_ASGARDEO_CLIENT_ID!,
   baseUrl: baseUrl,
   storage: "webWorker", 
 
@@ -13,9 +13,7 @@ export const authConfig:AuthReactConfig = {
     tokenEndpoint: "/oauth2/token",
     userinfoEndpoint: "/oauth2/userinfo",
     jwksUri: "/oauth2/jwks",
-
     revocationEndpoint: "/oauth2/revoke",
-
     checkSessionIframe: "/oidc/checksession",
     endSessionEndpoint: "/oidc/logout",
     issuer: baseUrl,
